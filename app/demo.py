c89b8476889cf277e8c8d1d1d38b838f4a16aa2a
David Petrov <david.petrov@ontotext.com>
2025-06-24 00:00:55 +0300

    Add extended prompts execution

--- app/demo.py
+++ app/demo.py
@@ -34,7 +34,7 @@ st.title("Entity Alignment Toolkit")
 
 task = st.sidebar.selectbox(
     "Select Task",
-    ["Candidates Finder", "Similarity Search", "LLM classification"]
+    ["Candidates Finder", "Similarity Search Evaluation", "LLM classification"]
 )
 
 
@@ -42,11 +42,12 @@ if task == "Candidates Finder":
     st.header("🔎 Candidates Finder")
 
     # Input: label to search
-    search_term = st.text_input("Enter entity label:", "")
+    search_term = st.text_input("Enter entity label:", "", help="Label of an entity to search equivalent for")
 
     # Input: index to use
     index_options = ["doid_labels", "mesh_labels"]
-    index_name = st.selectbox("Choose similarity index:", index_options)
+    index_name = st.selectbox("Choose similarity index:", index_options,
+                              help="Choose against which similarity index to search")
 
     # Optional: limit number of results
     top_k = st.slider("Number of candidates", 1, 50, 10)
@@ -76,17 +77,22 @@ if task == "Candidates Finder":
                 st.dataframe(styled_df, use_container_width=True)
 
 # Task 2: Similarity Search
-elif task == "Similarity Search":
+elif task == "Similarity Search Evaluation":
     st.header("📊 Similarity Search Evaluation")
 
     config = load_similarity_config()
     kg_pairs = list(config.keys())
-    selected_pair = st.selectbox("Knowledge Graphs Pair:", kg_pairs)
+    selected_pair = st.selectbox("Knowledge Graphs Pair:", kg_pairs,
+                                 help="Available knowledge graph pairs for evaluation with similarity search")
+
     selected_mapping = st.selectbox(
-        "Mappings:", config[selected_pair] if selected_pair else []
+        "Mappings:", config[selected_pair] if selected_pair else [],
+        help="Available seed mappings for the selected kg pair"
     )
 
-    save_candidates = st.checkbox("Save candidates data", value=True)
+    save_candidates = st.checkbox("Save candidates data", value=True,
+                                  help="Persists generated candidates data to be used by the LLM classification task")
+
     if st.button("Compute Hits@K"):
         with st.spinner("Computing Hits@K..."):
             hits_scores, mapping_size, elapsed_time = compute_hits_at_n(selected_pair, selected_mapping, save_candidates)
@@ -122,7 +128,8 @@ elif task == "LLM classification":
     # Saved candidates results
     candidates_results = load_candidate_results()
     candidates_results_names = [f.replace(".json","") for f in candidates_results if f.endswith(".json")]
-    candidates_result = st.selectbox("Choose candidates results:", candidates_results_names)
+    candidates_result = st.selectbox("Choose candidates results:", candidates_results_names,
+                                     help="Choose candidates results generated from the task Similarity Search.")
 
 
     # Assumes the string is always in the format source-target_mappings
@@ -132,7 +139,11 @@ elif task == "LLM classification":
 
     # Input: index to use
     model_options = ["gpt-4o-mini", "gpt-3.5-turbo","gpt-4o-2024-11-20" ]
-    model = st.selectbox("Choose OpenAI model:", model_options)
+    model = st.selectbox("Choose OpenAI model:", model_options,
+                         help="Select which OpenAI model to use for classification.")
+
+    extended_prompt = st.checkbox("Use extended prompt", value=False,
+                                  help="Uses more detailed prompts with all altLabel and parent classes for the source and all the candidates.")
 
     current_page = 1
 
@@ -170,16 +181,22 @@ elif task == "LLM classification":
         st.session_state.last_candidates_result = candidates_result
     if "last_model" not in st.session_state:
         st.session_state.last_model = model
+    if "last_extended_prompt" not in st.session_state:
+        st.session_state.last_extended_prompt = extended_prompt
 
     # Reset classified results if inputs changed
-    if (st.session_state.last_candidates_result != candidates_result or
-            st.session_state.last_model != model):
+    if (
+            st.session_state.last_candidates_result != candidates_result or
+            st.session_state.last_model != model or
+            st.session_state.last_extended_prompt != extended_prompt
+    ):
         st.session_state.classified_pages = {}  # Clear the cache
         st.session_state.current_page = 1  # Optionally reset to page 1
 
         # Update tracked inputs
         st.session_state.last_candidates_result = candidates_result
         st.session_state.last_model = model
+        st.session_state.last_extended_prompt = extended_prompt
 
 
     # Session state to cache results
@@ -190,7 +207,7 @@ elif task == "LLM classification":
     if st.button("Classify"):
         with st.spinner("Sending prompts to OpenAI..."):
             if st.session_state.current_page not in st.session_state.classified_pages:
-                result_df, score = llm_utils.classify(page_data, model, source, target)
+                result_df, score = llm_utils.classify(page_data, model, source, target, extended_prompt)
                 st.session_state.classified_pages[st.session_state.current_page] = (result_df, score)
             else:
                 result_df, score = st.session_state.classified_pages[st.session_state.current_page]
--- llm/llm_utils.py
+++ llm/llm_utils.py
@@ -2,7 +2,7 @@ import openai
 import os
 import json
 import pandas as pd
-
+from SPARQLWrapper import SPARQLWrapper, JSON
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(filename='.env'))
 
@@ -54,8 +54,95 @@ def create_prompt_with_candidates(source_ontology: str, target_ontology: str, so
     prompt += "\nIMPORTANT: Return only the ID, and nothing else. Do not explain."
     return prompt
 
+def get_extended_info(ontology: str, entity_id: str) -> dict:
+    """
+    Retrieve alternative and parent class labels from GraphDB using SPARQL queries loaded from file.
+    """
+    endpoint = "http://localhost:7200/repositories/WeVerify"
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setReturnFormat(JSON)
 
-def classify(data, model, source, target):
+    # Map ontology to query file
+    query_file_map = {
+        "icd10cm": "../queries/extended_info_icd10cm_mesh.rq",
+        "doid": "../queries/extended_info_doid.rq",
+        "mesh": "../queries/extended_info_icd10cm_mesh.rq"
+    }
+
+    ontology_key = ontology.lower()
+    if ontology_key not in query_file_map:
+        raise ValueError(f"Unsupported ontology: {ontology}")
+
+    # Load and inject entity ID into query
+    query_file = query_file_map[ontology_key]
+    with open(query_file, "r", encoding="utf-8") as f:
+        query_template = f.read()
+    query = query_template.replace("{{ENTITY_URI}}", entity_id)
+
+    # Execute the SPARQL query
+    sparql.setQuery(query)
+    results = sparql.query().convert()
+
+    # Parse result
+    bindings = results["results"]["bindings"]
+    if not bindings:
+        return {"altLabels": [], "parentClassLabels": []}
+
+    result = bindings[0]
+    alt_labels = result.get("altLabels", {}).get("value", "")
+    parent_labels = result.get("parentClassLabels", {}).get("value", "")
+
+    return {
+        "altLabels": [label.strip() for label in alt_labels.split(";") if label.strip()],
+        "parentClassLabels": [label.strip() for label in parent_labels.split(";") if label.strip()]
+    }
+
+
+def create_extended_prompt(source_ontology: str, target_ontology: str, source_id: str, info: dict ) -> str:
+    target_id_name = target_ontology.lower() + "_id"
+    source_extended_info = get_extended_info(source_ontology, source_id)
+
+    # Build optional label sections
+    alt_labels_section = (
+        f"- Alternative Labels: {'; '.join(source_extended_info['altLabels'])}"
+        if source_extended_info["altLabels"] else ""
+    )
+
+    parent_labels_section = (
+        f"- Parent Classes Labels: {'; '.join(source_extended_info['parentClassLabels'])}"
+        if source_extended_info["parentClassLabels"] else ""
+    )
+
+    # Join only non-empty sections
+    optional_sections = "\n".join(filter(None, [alt_labels_section, parent_labels_section]))
+
+    # Construct the final prompt
+    prompt = f"""You are comparing the ontologies {source_ontology} and {target_ontology}.
+
+    Given the source entity:
+    - ID: {source_id}
+    - Label: {info["label"]}
+    {optional_sections}
+
+    Here are candidate equivalent entities in {target_ontology}:
+    """
+
+
+    for c in info["candidates"]:
+        short_id = c[target_id_name].rsplit('/', 1)[-1]
+        candiadate_extended_info = get_extended_info(target_ontology, c[target_id_name])
+        prompt += f"- ID: {short_id}, Label: {c['label']}, String-similarity-score: {c['score']}"
+        if candiadate_extended_info["altLabels"]:
+            prompt+=f", Alternative Labels: {'; '.join(candiadate_extended_info['altLabels'])}"
+        if candiadate_extended_info["parentClassLabels"]:
+            prompt+=f", Parent Classes Labels: {'; '.join(candiadate_extended_info['parentClassLabels'])}"
+        prompt+="\n"
+
+    prompt += "\nWhich one is the best match?"
+    prompt += "\nIMPORTANT: Return only the ID, and nothing else. Do not explain."
+    return prompt
+
+def classify(data, model, source, target, extended_prompt):
     num_queries = len(data)
 
     target_prefix = target_prefixes[target]
@@ -69,7 +156,11 @@ def classify(data, model, source, target):
         equivalent_id = info.get("equivalent_id")
         equivalent_id_label = info.get("equivalent_id_label")
 
-        prompt = create_prompt_with_candidates(source, target, source_id, info)
+        prompt = (
+            create_extended_prompt(source, target, source_id, info)
+            if extended_prompt
+            else create_prompt_with_candidates(source, target, source_id, info)
+        )
         llm_answer = target_prefix +  find_equivalent_entity(model, prompt)
 
         if llm_answer == equivalent_id:
@@ -93,6 +184,10 @@ def classify(data, model, source, target):
 
 #Example usage
 if __name__ == "__main__":
+    # extended_info_icd = get_extended_info("icd10cm", "http://purl.bioontology.org/ontology/ICD10CM/J09.X")
+    # extended_info_doid = get_extended_info("doid", "http://purl.obolibrary.org/obo/DOID_8469")
+    # extended_info_mesh = get_extended_info("mesh", "http://purl.bioontology.org/ontology/MESH/D017889")
+
     file_path = "../data/candidates/ICD10CM-DOID_extended_mappings_doid_icd10cm.json"
 
     with open(file_path, "r", encoding="utf-8") as f:
@@ -110,9 +205,9 @@ if __name__ == "__main__":
     correct_cnt = 0
     missing_candidate_cnt = 0
 
-    model = "gpt-4o-mini"
-    model = "gpt-3.5-turbo"
-    # model = "gpt-4o-2024-11-20"
+    # model = "gpt-4o-mini"
+    # model = "gpt-3.5-turbo"
+    model = "gpt-4o-2024-11-20"
 
     for i, (source_id, info) in enumerate(candidates_data.items()):
         if i >= max_queries:
@@ -128,7 +223,7 @@ if __name__ == "__main__":
             missing_candidate_cnt += 1
             continue
 
-        prompt = create_prompt_with_candidates(source_ontology, target_ontology, source_id, info)
+        prompt = create_extended_prompt(source_ontology, target_ontology, source_id, info)
         print(prompt)
         llm_answer = target_prefix +  find_equivalent_entity(model, prompt)
 
